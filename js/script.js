8629a17ceaf8315a26a4692256aeb4e8c1577b8c
Marat Aylbekov <maratadylbekov@gmail.com>
2024-03-30 22:45:36 +0600

    practiceLessonOne

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt('How many movies have you watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('The last movie you watched?', ''),
+      b = prompt('How much you appreciate him?'),
+      c = prompt('The last movie you watched?', ''),
+      d = prompt('How much you appreciate him?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
