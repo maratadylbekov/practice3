fa0ea842c48a23d26142a3fc92d18f10069cc06d
Marat A. <maratadylbekov@gmail.com>
2024-09-26 17:59:27 +0600

    1st prct lssn

--- lesson12/js/script.js
+++ lesson12/js/script.js
@@ -1,1 +1,20 @@
 'use strict';
+let numberOfFilms = +prompt('How many movies you have seen?', '10');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('The last movies you watched?', ''),
+      b = prompt('Rate it', '1'),
+      c = prompt('The last movies you watched?', ''),
+      d = prompt('Rate it', '1');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
