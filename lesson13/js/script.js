248f5714cefc870988fd9eb1b8a5693bf640769d
Marat A. <maratadylbekov@gmail.com>
2024-09-27 16:33:33 +0600

    2nd prctc lssn

--- lesson13/js/script.js
+++ lesson13/js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {
+    console.log('Error!');
+}
+
+// const num = 50;
+if (num < 49) {
+    console.log('error');
+} else if (num > 100) {
+    console.log('too big')
+} else {
+    console.log('ok');
+}
+
+(num === 50) ? console.log('ok') : console.log('error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('incorrect');
+        break;
+    case 100:
+        console.log('too big');
+        break;
+    case 51:
+        console.log('correct');
+        break;
+    default:
+        console.log('try again');
+        break;
+}
--- lesson15/js/script.js
+++ lesson15/js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+let numberOfFilms = +prompt('How many movies you have seen?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('The last movies you watched?', ''),
+          b = prompt('Rate it', '1');
+
+    if (a != null && b != null && a != '' && b != ''&& a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done!');
+    } else {
+        console.log('error');
+        i--;
+    }    
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('not many');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('classic movie watcher');
+} else if (personalMovieDB.count >= 30) {
+    console.log('movie lover');
+} else {
+    console.log('error');
+}
+
+console.log(personalMovieDB);
